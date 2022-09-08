cc812c87438349f1fe0f7d9df4d26714cca2bdfd
joseMHernandez <manuhdez010@gmail.com>
2022-09-07 23:56:59 -0400

    adding navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className='nav'>
+        
+        <div className='nav-logo'>Logo</div>
+        
+        <div className='nav-link'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        </div>
+    </nav>
+    
+    </>
+  )
+}
+
+export default Navbar;
